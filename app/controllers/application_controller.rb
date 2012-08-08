2c45fe37ced5e729ceca917a0e116e73a8642b18
Andrew Zimmer <andrewzimmer906@gmail.com>
2012-08-08 16:23:56 -0600

    metas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :meta_defaults
+    private
+
+   def meta_defaults
+      @meta_title = "Deliver Beer To Me"
+      @meta_keywords = "Beer, Alcohol, Delivery, Service, Delivery Service, Party, Boulder"
+      @meta_description = "Don't drive! Order beer online and have it delivered to your front door in time to keep the party alive."
+    end
 end
